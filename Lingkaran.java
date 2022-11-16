5d203842a220a116ee256a091802b1275b584389
Raka Adriel <114383364+adrielrakaa@users.noreply.github.com>
2022-11-16 14:15:44 +0700

    Add files via upload

---
+++ Lingkaran.java
@@ -0,0 +1,20 @@
+package UTS;
+
+public class Lingkaran {
+    
+     private int jejari;
+     private double luas;
+      
+     public void setJejari(int r){
+         if (r > 0){
+             this.jejari = r;
+         } else {
+             this.jejari = 0;
+         }
+     }
+      
+     public double getLuas(){
+         this.luas = Math.PI * Math.pow(this.jejari, 2);
+         return this.luas;
+     }
+}
